0cd90a74d2b6281fa4af5194b04fc583b1e636f5
Junior Moraes <juniorfvox@gmail.com>
2017-09-01 17:55:19 -0300

    DONM-37: Endpoint de listagem de regiões.

---
+++ lib/Donm/Controller/API/Region.pm
@@ -0,0 +1,54 @@
+package Donm::Controller::API::Region;
+use common::sense;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'CatalystX::Eta::Controller::REST' }
+
+with "CatalystX::Eta::Controller::AutoBase";
+
+__PACKAGE__->config(
+    # AutoBase.
+    result => "DB::Region",
+);
+
+sub root : Chained('/api/root') : PathPart('') : CaptureArgs(0) { }
+
+sub base : Chained('root') : PathPart('region') : CaptureArgs(0) { }
+
+sub list : Chained('base') : PathPart('') : Args(0) : ActionClass('REST') { }
+
+sub list_GET {
+    my ($self, $c) = @_;
+
+    my $region_rs = $c->stash->{collection}->search(
+        {},
+        {
+            '+select' => [ \"ST_ASGEOJSON(ST_TRANSFORM(ST_SIMPLIFY(ST_TRANSFORM(geom, 2249), 500), 4326), 6)" ],
+            '+as'     => [ qw(geo_json) ],
+        }
+    );
+
+    return $self->status_ok(
+        $c,
+        entity => {
+            region => [
+                map {
+                    my $r = $_;
+                    +{
+                        map { $_ => $r->get_column($_) }
+                        qw/
+                        id
+                        name
+                        geo_json
+                        /
+                    }
+                } $region_rs->all()
+            ],
+        },
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ t/api/001-region.t
@@ -0,0 +1,19 @@
+use common::sense;
+use FindBin qw($Bin);
+use lib "$Bin/../lib";
+use Donm::Test::Further;
+
+my $schema = Donm->model("DB");
+
+db_transaction {
+    rest_get "/api/region", name => "list regions", stash => "region";
+
+    stash_test "region" => sub {
+        my $res = shift;
+
+        is( ref($res), "HASH", 'returns an hashref' );
+        is( scalar @{ $res->{region} }, 96, 'count=96' );
+    };
+};
+
+done_testing();
