16f3f641fa826119747a613da9981d35610a319a
Junior Moraes <juniorfvox@gmail.com>
2017-10-02 17:56:39 -0300

    DONM-113: Get specific district.

--- lib/Donm/Controller/API/Region.pm
+++ lib/Donm/Controller/API/Region.pm
@@ -6,15 +6,59 @@ use namespace::autoclean;
 BEGIN { extends 'CatalystX::Eta::Controller::REST' }
 
 with "CatalystX::Eta::Controller::AutoBase";
+with "CatalystX::Eta::Controller::AutoResultGET";
 
 __PACKAGE__->config(
     # AutoBase.
     result => "DB::Region",
+
+    # AutoResultGET.
+    object_key => "region",
+    build_row  => sub {
+        my ($region, $self, $c) = @_;
+
+        return {
+            region => {
+                ( map { $_ => $region->$_ } qw/ id name subprefecture_id / ),
+
+                subprefecture => +{
+                    map { $_ => $region->subprefecture->$_ }
+                      qw/ id acronym name latitude longitude site email telephone address /
+                },
+
+                region_variables => [
+                    map {
+                        my $rv = $_;
+                        +{
+                            ( map { $_ => $rv->$_ } qw/ id region_id variable_id value / ),
+
+                            variable => +{ map { $_ => $rv->variable->$_ } qw/ id name / },
+                        };
+                    } $region->region_variables->all()
+                ],
+            }
+        };
+    },
 );
 
 sub root : Chained('/api/root') : PathPart('') : CaptureArgs(0) { }
 
 sub base : Chained('root') : PathPart('region') : CaptureArgs(0) { }
+
+sub object : Chained('base') : PathPart('') : CaptureArgs(1) {
+    my ($self, $c, $region_id) = @_;
+
+    $c->stash->{region_id} = $region_id;
+
+    my $region_rs = $c->stash->{collection}->search(
+        {},
+        { prefetch => [ { "region_variables" => "variable" } ] },
+    );
+
+    if ( !( $c->stash->{region} = $region_rs->search( { 'me.id' => $region_id } )->next ) ) {
+        $c->detach("/error_404");
+    }
+}
 
 sub list : Chained('base') : PathPart('') : Args(0) : ActionClass('REST') { }
 
@@ -48,6 +92,10 @@ sub list_GET {
         },
     );
 }
+
+sub result : Chained('object') : PathPart('') : Args(0) : ActionClass('REST') { }
+
+sub result_GET { }
 
 __PACKAGE__->meta->make_immutable;
 
--- t/api/001-region.t
+++ t/api/001-region.t
@@ -6,14 +6,32 @@ use Donm::Test::Further;
 my $schema = Donm->model("DB");
 
 db_transaction {
-    rest_get "/api/region", name => "list regions", stash => "region";
+    rest_get "/api/region", name => "list regions", stash => "regions";
 
-    stash_test "region" => sub {
+    stash_test "regions" => sub {
         my $res = shift;
 
         is( ref($res), "HASH", 'returns a hashref' );
         is( scalar @{ $res->{regions} }, 96, 'count=96' );
     };
+
+    rest_get "/api/region/630",
+        name  => "get region",
+        stash => "region";
+    ;
+
+    stash_test "region" => sub {
+        my $res = shift;
+
+        is( $res->{region}->{id}, 630, 'id=630' );
+        is( $res->{region}->{name}, 'Sacomã', 'name=Sacomã' );
+        is( $res->{region}->{subprefecture_id}, 11, 'subprefecture_id=11' );
+        is( $res->{region}->{subprefecture}->{id}, 11, 'subprefecture_id=11' );
+        is( $res->{region}->{subprefecture}->{name}, 'Ipiranga', 'subprefecture_name=Ipiranga' );
+        is( ref $res->{region}->{region_variables}, 'ARRAY', 'region_variables=ARRAY' );
+        is( $res->{region}->{region_variables}->[0]->{variable_id}, 19, 'variable_id=19' );
+
+    }
 };
 
 done_testing();
