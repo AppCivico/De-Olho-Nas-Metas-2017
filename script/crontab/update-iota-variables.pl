6305255ce41118a687f9c530ab6af0ffd80ca677
Junior Moraes <juniorfvox@gmail.com>
2017-12-07 16:22:37 -0200

    DONM-243: Add source and year to variables

--- script/crontab/update-iota-variables.pl
+++ script/crontab/update-iota-variables.pl
@@ -4,7 +4,6 @@ use open ":locale";
 use FindBin qw($RealBin $Script);
 use lib "$RealBin/../../lib";
 
-use DDP;
 use Text::CSV;
 use Tie::Handle::CSV;
 use JSON::XS;
@@ -12,6 +11,7 @@ use DateTime::Format::Strptime;
 use Donm::SchemaConnected;
 use Log::Log4perl qw(:easy);
 use LWP::UserAgent;
+use DDP;
 
 # Log.
 Log::Log4perl->easy_init({
@@ -26,6 +26,7 @@ Log::Log4perl->easy_init({
     utf8   => 1,
 });
 
+
 INFO "Iniciando $Script...";
 
 my $schema = get_schema();
@@ -35,12 +36,70 @@ my %variables = (
     27 => 'Área total do município em quilômetros quadrados (km²)',
     19 => 'População total',
 
-    # TODO Verificar se a cidade de São Paulo possui estas variáveis abaixo.
-    #2260 => 'Renda per capita',
-    #1839 => 'População (IBGE)',
-    #3037 => 'Renda per capita média mensal',
-    #1802 => 'Densidade demográfica do município',
-    #3192 => 'IDH',
+    # Assistencia social.
+    512 => "Número total da população em situação de rua que foi acolhida",
+    285 => "Número total de indivíduos em situação de rua",
+
+    # Cultura.
+    250 => "Número de livros infanto-juvenis disponíveis em acervos de bibliotecas",
+    71  => "Número total de livros para adultos disponíveis em bibliotecas municipais",
+    249 => "Número de centros culturais, espaços e casas de cultura",
+    572 => "Número total de salas de cinema",
+    555 => "Ńúmero total de museus",
+    573 => "Número total de salas de show e concerto",
+    575 => "Número total de salas de teatro",
+
+    # Educação.
+    77  => "Número de matrículas efetuadas em creches municipais",
+    78  => "Número total de inscritos em creches (matrículas efetuadas + vagas solicitadas)",
+    269 => "Número total de matrículas  em pré-escolas municipais",
+    270 => "Número total de inscritos em pré-escolas municipais (matrículas efetuadas + vagas solicitadas)",
+
+    # Esporte.
+    60  => "Número total de equipamentos esportivos públicos",
+    577 => "Número total de unidades esportivas públicas",
+
+    # Habitação.
+    545 => "Número total de domicílios em favelas",
+    92  => "Número total de indivíduos residentes em favelas",
+
+    # Inclusão digital.
+    576 => "Número total de telecentros",
+
+    # Meio ambiente.
+    61 => "Número total, em m², de áreas verdes",
+
+    # Saúde.
+    231 => "Número total de crianças com baixo peso ao nascer",
+    58 => "Número total de leitos hospitalares",
+    3767 => "Número de óbitos de residentes por aids",
+    230 => "Número total de óbitos por causas maternas",
+    557 => "Número total de óbitos por causas externas",
+    558 => "Número total de óbitos por causas mal definidas",
+    227 => "Número total de óbitos por doenças do aparelho circulatório",
+    226 => "Número total de óbitos por doenças do aparelho respiratório",
+    508 => "Número de óbitos por neoplasias",
+    3937 => "Idade média ao morrer",
+    57 => "Número total de unidades básicas de atendimento em saúde",
+
+    # Trabalho e renda.
+    548 => "Número total de empregos",
+    72 => "Taxa média de desemprego",
+
+    # Transporte/Acidentes de trânsito
+    294 => "Total de mortes de ocupantes de automóveis e caminhonetes",
+    293 => "Total de mortes de ocupantes de motocicleta",
+    214 => "Total de mortes com bicicleta",
+    296 => "Número total de atropelamentos",
+    91 => "Número de mortes em acidentes de trânsito",
+
+    # Violência.
+    277 => "Número total de internações de crianças residentes, de até 14 anos por causas relacionadas a possíveis agressões",
+    279 => "Número total de internações de pessoas residentes, de 60 anos ou mais, por causas relacionadas a possível agressão",
+    280 => "Número total de internações de mulheres residentes, de 20 a 59 anos, por causas relacionadas a possíveis agressões",
+    81 => "Número total de mortes por homicídio de jovens homens com idade de 15 a 29 anos",
+    82 => "Número total de óbitos por homicídio",
+    83 => "Número total de roubos",
 );
 
 for my $variable_id (keys %variables) {
@@ -50,7 +109,7 @@ for my $variable_id (keys %variables) {
 
     $schema->resultset("Variable")->find_or_create(
         {
-            id => $variable_id,
+            id   => $variable_id,
             name => $variable_name,
         },
         { key => "primary" },
@@ -80,7 +139,6 @@ $ua->env_proxy();
 $ua->default_header( 'User-Agent' => "AppCivico/1.0" );
 
 # Atualizando regiões.
-# http://www.redesocialdecidades.org.br/download-variables?city_id=1&region_id=288&variable_id=1839
 for my $region_id (keys %regions) {
     my $region_name = $regions{$region_id};
 
@@ -114,9 +172,11 @@ for my $region_id (keys %regions) {
                 my $most_recent_data = pop @data;
                 my $value = $most_recent_data->{value};
 
-                my $region = $schema->resultset("Region")->search( { name => { 'ilike', $region_name } } )->next;
+                my $dt = $strp->parse_datetime($most_recent_data->{valid_from});
+
+                my $region = $schema->resultset("Region")->search( { name => { 'ilike' => $region_name } } )->next;
                 if (ref $region) {
-                    my $region_variable_rs = $schema->resultset("RegionVariable");
+                    my $region_variable_rs = $schema->resultset('RegionVariable');
 
                     my $region_variable = $region_variable_rs->search(
                         {
@@ -125,8 +185,16 @@ for my $region_id (keys %regions) {
                         }
                     )->next;
 
+                    my $source = $most_recent_data->{source};
+
                     if (ref $region_variable) {
-                        $region_variable->update( { value => $value } );
+                        $region_variable->update(
+                            {
+                                value  => $value,
+                                year   => $dt->year,
+                                source => $source,
+                            }
+                        );
                     }
                     else {
                         $region_variable_rs->create(
@@ -134,6 +202,8 @@ for my $region_id (keys %regions) {
                                 region_id   => $region->id,
                                 variable_id => $variable_id,
                                 value       => $value,
+                                year        => $dt->year,
+                                source      => $source,
                             }
                         );
                     }
