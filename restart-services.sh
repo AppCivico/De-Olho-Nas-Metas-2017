f80f465210efb8249b1e276e64c72d06f405007b
Junior Moraes <juniorfvox@gmail.com>
2018-01-08 18:00:36 -0200

    Add hot deployment to docker image

---
+++ restart-services.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export PIDFILE=/tmp/start_server.pid;
+
+cd /src;
+source /home/app/perl5/perlbrew/etc/bashrc;
+source envfile.sh;
+
+if [ -e "$PIDFILE" ]; then
+    kill -HUP $(cat $PIDFILE)
+fi
+
---
+++ sample-gracefully-reload.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker exec -u app donm2017 /src/restart-services.sh
