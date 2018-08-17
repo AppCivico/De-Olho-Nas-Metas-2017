d5ac21945383ad40d76eed598cda6f27376129e7
Junior Moraes <carlos@eokoe.com>
2018-08-17 11:49:27 -0300

    Update subprefecture names on sqitch
    DONM-273 #time 20m

--- schema/deploy/0036-planejasampa-api.sql
+++ schema/deploy/0036-planejasampa-api.sql
@@ -27,4 +27,25 @@ CREATE TABLE goal_execution (
     UNIQUE(goal_id, period, accumulated)
 );
 
+UPDATE subprefecture SET name = 'A definir' WHERE id = 35;
+UPDATE subprefecture SET name = 'Vila Maria/Vila Guilherme' WHERE id = 29;
+UPDATE subprefecture SET name = 'Capela do Socorro' WHERE id = 4;
+UPDATE subprefecture SET name = 'Casa Verde' WHERE id = 5;
+UPDATE subprefecture SET name = 'Freguesia/Brasilândia' WHERE id = 9;
+UPDATE subprefecture SET name = 'Jaçanã/Tremembé' WHERE id = 15;
+UPDATE subprefecture SET name = 'M''Boi Mirim' WHERE id = 17;
+UPDATE subprefecture SET name = 'Pirituba/Jaraguá' WHERE id = 23;
+UPDATE subprefecture SET name = 'Santana/Tucuruvi' WHERE id = 24;
+UPDATE subprefecture SET name = 'São Miguel Paulista' WHERE id = 27;
+
+CREATE TABLE goal_execution_subprefecture (
+    id                SERIAL PRIMARY KEY,
+    goal_id           INTEGER NOT NULL REFERENCES goal(id),
+    subprefecture_id  INTEGER NOT NULL REFERENCES subprefecture(id),
+    period      INTEGER NOT NULL,
+    value       TEXT NOT NULL,
+    updated_at  TIMESTAMP WITHOUT TIME ZONE,
+    UNIQUE(subprefecture_id, goal_id, period)
+);
+
 COMMIT;
