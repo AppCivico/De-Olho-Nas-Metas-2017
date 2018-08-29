413fa27b4ba93a9b3899a3326a107c75e199232d
Junior Moraes <carlos@eokoe.com>
2018-08-29 14:17:28 -0300

    Deal with negative projection values
    DONM-315 #time 25m

--- lib/Donm/Schema/Result/GoalExecution.pm
+++ lib/Donm/Schema/Result/GoalExecution.pm
@@ -181,11 +181,7 @@ sub get_progress {
     my $self = shift;
 
     # Projeção.
-    my $year = $self->get_year();
-    my $projection = $year =~ m{^201[78]$}
-        ? $self->goal->get_column('projection_first_biennium')
-        : $self->goal->get_column('projection_second_biennium')
-    ;
+    my $projection = $self->goal->get_column('projection_second_biennium');
 
     # Normalizando os dados de acordo com a projeção.
     ($projection) = split m{\n}, $projection;
@@ -261,12 +257,23 @@ sub get_progress {
     my $value = $self->get_column('value');
     $value =~ s/^\s+|\s+$//g;
 
-    my $projection_base_diff = $projection - $base_value;
-    my $value_base_diff      = $value - $base_value;
+    if ($projection >= $base_value) {
+        my $projection_base_diff = $projection - $base_value;
+        my $value_base_diff      = $value - $base_value;
 
-    $projection_base_diff ||= 1; # Avoid illegal division by zero.
+        $projection_base_diff ||= 1; # Avoid illegal division by zero.
 
-    return sprintf('%.2f', ( ( $value_base_diff * 100 ) / $projection_base_diff ));
+        return sprintf('%.2f', ( ( $value_base_diff * 100 ) / $projection_base_diff ));
+    }
+    else {
+        my $projection_base_diff = $base_value - $projection;
+        my $value_base_diff = $value - $projection;
+
+        $projection_base_diff ||= 1; # Avoid illegal division by zero.
+        return sprintf('%.2f', ( 100 - ( ( $value_base_diff * 100 ) / $projection_base_diff ) ));
+    }
+
+    return undef; ## no critic
 }
 
 __PACKAGE__->meta->make_immutable;
